b388674c0b8deeb77b867b7b96f25ec172ab1bb3
zcs0228 <zcs0228@yahoo.com>
2015-08-01 22:11:19 +0800

    创建初次提交

---
+++ WatchDog/WatchDog/DECAllFileEncryption.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WatchDog
+{
+    public class DECAllFileEncryption
+    {
+        private string _password;
+
+        public DECAllFileEncryption(string password)
+        {
+            _password = password;
+        }
+
+        /// <summary>
+        /// 加密文件夹中所有文件
+        /// </summary>
+        /// <param name="sourceDirectoryInfo">需要加密的文件夹</param>
+        /// <param name="targetPath">存储加密结果的文件夹</param>
+        public void EncryptAllFiles(DirectoryInfo sourceDirectoryInfo, string targetPath)
+        {
+            //在目标文件夹下创建源文件根目录文件夹
+            string myTargetPath = targetPath + "\\" + sourceDirectoryInfo.Name;
+            if (!Directory.Exists(myTargetPath))
+            {
+                Directory.CreateDirectory(myTargetPath);
+            }
+
+            //获取目标文件夹中的子文件夹
+            DirectoryInfo[] dirInfos = sourceDirectoryInfo.GetDirectories();
+            foreach (DirectoryInfo item in dirInfos)
+            {
+                EncryptAllFiles(item, myTargetPath);
+            }
+
+            //获取目标文件夹中的子文件
+            FileInfo[] fileInfos = sourceDirectoryInfo.GetFiles();
+            foreach (FileInfo item in fileInfos)
+            {
+                DECFileEncryption.EncryptFile(item.FullName, myTargetPath + "\\" + item.Name, _password);
+            }
+        }
+
+        /// <summary>
+        /// 解密文件夹中所有文件
+        /// </summary>
+        /// <param name="sourceDirectoryInfo">需要解密的文件夹</param>
+        /// <param name="targetPath">存储解密结果的文件夹</param>
+        public void DecryptAllFiles(DirectoryInfo sourceDirectoryInfo, string targetPath)
+        {
+            //在目标文件夹下创建源文件根目录文件夹
+            string myTargetPath = targetPath + "\\" + sourceDirectoryInfo.Name;
+            if (!Directory.Exists(myTargetPath))
+            {
+                Directory.CreateDirectory(myTargetPath);
+            }
+
+            //获取目标文件夹中的子文件夹
+            DirectoryInfo[] dirInfos = sourceDirectoryInfo.GetDirectories();
+            foreach (DirectoryInfo item in dirInfos)
+            {
+                DecryptAllFiles(item, myTargetPath);
+            }
+
+            //获取目标文件夹中的子文件
+            FileInfo[] fileInfos = sourceDirectoryInfo.GetFiles();
+            foreach (FileInfo item in fileInfos)
+            {
+                DECFileEncryption.DecryptFile(item.FullName, myTargetPath + "\\" + item.Name, _password);
+            }
+        }
+    }
+}
---
+++ WatchDog/WatchDog/Main.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Transactions;
+using System.Windows.Forms;
+
+namespace WatchDog
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                this.txtselectedFile.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            string sourceFileName = "";
+            string code = "";
+            string savefileName = "";
+            if (this.txtselectedFile.Text.Trim() != "")
+            {
+                sourceFileName = this.txtselectedFile.Text.Trim();
+            }
+            else
+            {
+                MessageBox.Show("请选择加密文件！");
+                return;
+            }
+            if (this.txtCode.Text.Trim() != "")
+            {
+                code = this.txtCode.Text.Trim();
+            }
+            else
+            {
+                MessageBox.Show("请输入密码！");
+                return;
+            }
+
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择保存路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                savefileName = dialog.SelectedPath;
+            }
+            else
+            {
+                return;
+            }
+
+            using (TransactionScope scope = new TransactionScope())
+            {
+                DECAllFileEncryption decAll = new DECAllFileEncryption(code);
+                DirectoryInfo sourceInfo = new DirectoryInfo(sourceFileName);
+                try
+                {
+                    decAll.EncryptAllFiles(sourceInfo, savefileName);
+                    scope.Complete();
+                    MessageBox.Show("加密成功！");
+                    this.txtCode.Text = "";
+                    this.txtselectedFile.Text = "";
+                }
+                catch
+                {
+                    scope.Dispose();
+                    MessageBox.Show("加密失败！");
+                }
+            }
+        }
+
+        private void btnDectypt_Click(object sender, EventArgs e)
+        {
+            string sourceFileName = "";
+            string code = "";
+            string savefileName = "";
+            if (this.txtselectedFile.Text.Trim() != "")
+            {
+                sourceFileName = this.txtselectedFile.Text.Trim();
+            }
+            else
+            {
+                MessageBox.Show("请选择解密文件！");
+                return;
+            }
+            if (this.txtCode.Text.Trim() != "")
+            {
+                code = this.txtCode.Text.Trim();
+            }
+            else
+            {
+                MessageBox.Show("请输入密码！");
+                return;
+            }
+
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件保存路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                savefileName = dialog.SelectedPath;
+            }
+            else
+            {
+                return;
+            }
+
+            using (TransactionScope scope = new TransactionScope())
+            {
+                DECAllFileEncryption decAll = new DECAllFileEncryption(code);
+                DirectoryInfo sourceInfo = new DirectoryInfo(sourceFileName);
+                try
+                {
+                    decAll.DecryptAllFiles(sourceInfo, savefileName);
+                    scope.Complete();
+                    MessageBox.Show("解密成功！");
+                    this.txtCode.Text = "";
+                    this.txtselectedFile.Text = "";
+                }
+                catch
+                {
+                    scope.Dispose();
+                    MessageBox.Show("解密失败！");
+                }
+            }
+        }
+    }
+}
